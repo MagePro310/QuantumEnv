d875fcfefee363ff7dadd404fa6b1c925f9d1389
MagePro310 <102444570+MagePro310@users.noreply.github.com>
2024-11-17 17:30:22 +0700

    Refactor result reconstruction: add reconstruct_counts_from_job function and update imports

--- src/tools/reconstructing.py
+++ src/tools/reconstructing.py
@@ -1,26 +1,51 @@
 """Result reconstruction using CKT toolbox."""
+from collections import Counter
+
 from circuit_knitting.cutting import reconstruct_expectation_values
 from qiskit.primitives.sampler import SamplerResult
 
-from src.common import Experiment
+from src.common import Experiment, CircuitJob, CombineJob
 
-def reconstruct_counts(experiment: Experiment) -> None:
+def reconstruct_counts_from_job(job: CombineJob) -> list[CircuitJob]:
     """_summary_
 
     Args:
-        experiment (Experiment): _description_
+        job (CombinedJob): _description_
+
+    Returns:
+        list[CircuitJob]: _description_
     """
-    quasi_distributions = []
-    for counts in experiment.result_counts:
-        quasi_distribution = {}
-        for bits, count in counts.items():
-            quasi_distribution[int(bits.replace(" ", ""), 2)] = count
-        quasi_distributions.append(quasi_distribution)
-    metadata = []
-    for circuit in experiment.circuits:
-        metadata.append({"num_qpd_bits": len(circuit.cregs[0])})
-    result = SamplerResult(quasi_distributions, metadata)
-    return result
+    circuit_jobs = []
+    offset = 0
+    for idx, _ in enumerate(job.indices):
+        mapping = job.mapping[idx]
+        observable = job.observable[0][mapping]
+        counts = _get_partial_counts(job.result_counts, offset, job.cregs[idx])
+        circuit_jobs.append(
+            CircuitJob(
+                job.indices[idx],
+                None,
+                job.coefficients[idx],
+                job.n_shots,
+                observable,
+                job.partition_labels[idx],
+                counts,
+                job.uuids[idx],
+                job.cregs[idx],
+            )
+        )
+        offset += job.cregs[idx]
+    return circuit_jobs
+        
+        
+def _get_partial_counts(
+    counts: dict[str, int], offset: int, cregs: int
+) -> dict[str, int]:
+    partial_counts = Counter()
+    for bits, count in counts.items():
+        current_bits = " ".join(bits[::-1].split(" ")[offset : offset + cregs][::-1])
+        partial_counts[current_bits] += count
+    return dict(partial_counts)
 
 
 def reconstruct_expvals(experiments: list[Experiment]) -> list[float]:
