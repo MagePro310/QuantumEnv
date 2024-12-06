5d07c8c90ab950d847efd647956f9df33b7ff611
MagePro310 <trieu.nguyenxuan@hcmut.edu.vn>
2024-12-06 10:13:45 +0700

    Refactor benchmark functions: streamline docstrings, enhance readability, and optimize setup time calculations

--- data/benchmark/benchmark.py
+++ data/benchmark/benchmark.py
@@ -18,20 +18,12 @@ from .types import Result
 
 
 def _generate_batch(max_size: int, circuits_per_batch: int) -> list[QuantumCircuit]:
-    """Generates a batch of random circuits.
-    
-    Args:
-        max_size: maximum size of the circuits
-        circuits_per_batch: number of circuits per batch
-    
-    Returns:
-        batch of circuits
-    """
+    # Generate a random circuit
     batch = []
     for _ in range(circuits_per_batch):
-        size = np.random.randint(2, max_size + 1)                                       # size: number of qubits in the circuit 
-        circuit = get_benchmark(benchmark_name="random", level=0, circuit_size=size)    # get a random benchmark circuit
-        batch.append(circuit)                                                           # add the circuit to the batch
+        size = np.random.randint(2, max_size + 1)
+        circuit = get_benchmark(benchmark_name="random", level=0, circuit_size=size)
+        batch.append(circuit)
 
     return batch
 
@@ -42,17 +34,7 @@ def run_experiments(
     t_max: int,
     num_batches: int,
 ) -> list[dict[str, Collection[Collection[str]]]]:
-    """Runs the benchmakr experiments.
-    
-    Args:
-        circuits_per_batch: number of circuits per batch
-        settings: settings
-        t_max: maximum time
-        num_batches: number of batches
-        
-    Returns:
-        results
-    """
+    """Runs the benchmakr experiments."""
     results = []
     for setting in settings:
         max_size = max(setting.values())
@@ -82,9 +64,11 @@ def run_experiments(
             makespan, jobs = generate_extended_schedule(lp_instance, p_times, s_times)
             t_3 = perf_counter()
             result["extended"] = Result(makespan, jobs, t_3 - t_2)
-            benchmark_results.append(result)
+            benchmark_results.append(
+                {"results": result, "s_times": s_times, "p_times": p_times}
+            )
 
-            results.append({"setting": setting, "benchmarks": benchmark_results, "s_times": s_times, "p_times": p_times})
+        results.append({"setting": setting, "benchmarks": benchmark_results})
     return results
 
 
@@ -92,40 +76,20 @@ def _get_processing_times(
     base_jobs: list[QuantumCircuit],
     accelerators: dict[str, int],
 ) -> list[list[float]]:
-    """Get processing times for the jobs.
-    
-    Args:
-        base_jobs: base jobs
-        accelerators: accelerators
-        
-    Returns:
-        processing times
-    """
     return [
         [np.random.random() * 10 + job.num_qubits / 5 for _ in accelerators]
-        for job in base_jobs                  #create a list of processing times for each job by iterating over the base jobs and generating a random processing time for each accelerator
+        #[np.random.randint(0,3) + job.num_qubits //2 for _ in accelerators]
+        for job in base_jobs
     ]
 
 
 def _get_setup_times(
     base_jobs: list[QuantumCircuit], accelerators: dict[str, int], default_value: float
 ) -> list[list[list[float]]]:
-    """Get setup times for the jobs.
-    
-    Args:
-        base_jobs: base jobs
-        accelerators: accelerators
-        default_value: default value
-    
-    Returns:
-        setup times
-    """
     return [
         [
             [
-                default_value
-                if id_i in [id_j, 0]
-                else _calc_setup_times(job_i, job_j)
+                default_value if id_i in [id_j, 0] else _calc_setup_times(job_i, job_j)
                 for _ in accelerators
             ]
             for id_i, job_i in enumerate([None] + base_jobs)
@@ -139,4 +103,5 @@ def _calc_setup_times(
 ) -> float:
     if job_j is None:
         return 0.0
-    return np.random.random() * 10 + (job_i.num_qubits + job_j.num_qubits) / 10  #return a random setup time based on the number of qubits in the two jobs
+    return np.random.random() * 10 + (job_i.num_qubits + job_j.num_qubits) / 10
+    #  return np.random.randint(0,2) + (job_i.num_qubits + job_j.num_qubits) // 8
